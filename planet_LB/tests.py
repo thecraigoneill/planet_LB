6badef1893ce0143f570a87e524d3e0d1d07d7ed
Craig O'Neill <craig@192-168-1-9.tpgi.com.au>
2023-02-03 12:17:46 +1100

    Modded tests

--- planet_LB/tests.py
+++ planet_LB/tests.py
@@ -3,29 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special
 
-# Initialise constants and object
-M2S = p_LB.Myr2s
-pLB = p_LB.planet_LB()
+def test1:
+    # Initialise constants and object
+    M2S = p_LB.Myr2s
+    pLB = p_LB.planet_LB()
 
-#Time discretisation
-age = 50 #Age in Myrs
-tstep=2000 #Number of timesteps
-dt = age*M2S/tstep
+    #Time discretisation
+    age = 50 #Age in Myrs
+    tstep=2000 #Number of timesteps
+    dt = age*M2S/tstep
 
-#Mesh discretisation
-m=400  # Number of x divisions
-dx=500.0 #Size of grid spacing
+    #Mesh discretisation
+    m=400  # Number of x divisions
+    dx=500.0 #Size of grid spacing
 
 
-#Initial/boundary and heating properties
-H=0. # internal heat production
-Tinit = 1300 # Initial temperature
-TSurf = 0.0 #Surface temperature
+    #Initial/boundary and heating properties
+    H=0. # internal heat production
+    Tinit = 1300 # Initial temperature
+    TSurf = 0.0 #Surface temperature
 
-# Simulation 1
+    # Simulation 1
 
-LB = pLB.init_1D_LB(dt,dx,tstep,m,Tinit,TSurf,H)
-T=pLB.LB1D(LB)
-#print(LB['T'])
-print(T)
+    LB = pLB.init_1D_LB(dt,dx,tstep,m,Tinit,TSurf,H)
+    T=pLB.LB1D(LB)
+    #print(LB['T'])
+    print(T)
 
